5296f435210de9a9d3017c24700579b3fe2195fe
Lorenzo Magro <magrolorenzo@gmail.com>
2020-11-05 16:23:28 +0100

    non funziona append su html

---
+++ assets/main.js
@@ -0,0 +1,135 @@
+// milestone 1:
+// definire un array di oggetti; ogni oggetto rappresenta un'icona,
+// che è caratterizzata da: nome, prefisso, tipo e famiglia.
+// Utilizzando la funzione forEach e il template literal,
+// visualizzare in pagina tutte le icone con il proprio nome.
+
+// milestone 2:
+// definire un array di colori e associare ad ogni tipo di icona un colore.
+// Visualizzare le icone di colore diverso in base al tipo.
+
+// milestone 3:
+// aggiungere una select per filtrare le icone in base al tipo.
+// Popolare le options della select dinamicamente
+// e, ogni volta che cambia il valore selezionato, visualizzare le icone corrispondenti.
+
+// ******************************
+
+// Array copiato da repo di Sofia
+const icons = [
+    {
+        name: 'cat',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'crow',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dog',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dove',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dragon',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'horse',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'hippo',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'fish',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'carrot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'apple-alt',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'lemon',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'pepper-hot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'user-astronaut',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-graduate',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-ninja',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-secret',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    }
+];
+
+// Template del elemento html icona  --->  <i class="fas fa-apple-alt"></i>
+
+icons.forEach((icon) => {
+
+    // Per ogni icona mi salvo prefix, family e name per ricreare la stringa html
+    let {family, prefix, name} = icon;
+    let html_string = '<i class="' + family + ' ' + prefix + name + '"></i>';
+
+    console.log(html_string);
+
+    $('#icon-wrapper').append(`
+        <div class="icon">
+            ${html_string}
+            <p>${name}</p>
+        </div>
+    `);
+
+});
